e8e2167efb0b0b52dc20cd80b1d91af28c2e32db
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-11 09:25:38 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,66 @@
+# <<<<<<<<   DICITIONARY   >>>>>>>
+# pairs = ('key':value)
+# Duplicate key is not allowed but duplicate value is allowed
+
+
+# marks = {'mohit':90,'rohit':69,'rokcy':89,'keshav':99}
+# print(type(marks))
+# print(marks)
+
+# The key is passed in [] brackets to get the value
+# print(marks['mohit'])
+
+# Updation of the value in dictionary
+# marks['mohit'] = 65
+# print(marks['mohit'])
+
+# If the key is not present then it will be inserted in the dictionary
+# marks['rahul']=40 # Insertion
+# print(marks)
+
+# marks = {'mohit':90,'rohit':69,'rokcy':89,'keshav':99}
+
+# .keys() method is used to access only keys in dictionary
+# print(marks.keys())
+
+# .values() method is used to access only values in dictionary
+# print(marks.values())
+
+# pop() method is used to remove the pair by passing the key of that pair
+# marks.pop('mohit')
+# print(marks)
+
+# Removing the name ritu and marks 336 from the Dictionary
+# students = {'Name':['rahul','mohit','ritu','yash','jugnu'],
+#             'Marks':[52,14,36,45,96],
+#             'Subject':"Science"}
+# students['Marks'][1]=41
+# print(students)
+
+
+# ____________TYPECASTING__________
+
+
+# The changing of the varaible's datatype fron int -> float is called TYPECASTING
+
+# Int(), float(), str(), tuple(), list(), dict(), set()
+# num1 = 10 # Int
+# num1=float(num1) # Changing to the Float data type
+# print(num1)
+# print(type(num1))
+
+
+# <<<<<<<<<<<  OPERATORS  >>>>>>>>>>>
+
+
+# ________ARITHEMETIC OPERATORS________
+
+# num1=int(input("Enter number 1:"))
+# num2=int(input("Enter number 2:"))
+# print("num1 + num2: ",num1+num2) # Addition Operator
+# print("num1 - num2: ",num1-num2) # Subtraction Operator
+# print("num1 * num2: ",num1*num2) # Multiplication Operator
+# print("num1 / num2: ",num1/num2) # Division Operator
+# print("num1 % num2: ",num1%num2) # Modulus Operator
+# print("num1 // num2: ",num1//num2) # Floor Division Operator
+# print("num1 ** num2: ",num1**num2) # Power Operator
