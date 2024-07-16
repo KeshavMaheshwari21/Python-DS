d752eed37e77fc47d6c225453c7baa0aa1c33ec4
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-16 09:37:23 +0530

    Add files via upload

---
+++ day 7/manager.py
@@ -0,0 +1,22 @@
+manager_name="Dharampal Ji"
+address="Bihar"
+
+employe_list=['rohit','rahul','mohit','suresh']
+
+rohit_id = "hello@gmail.com"
+rohit_pass = "12345"
+
+
+def rohit_hi():
+    print("Employee name : Rohit")
+    print("Rohit id : ",rohit_id)
+    print("Rohit password : ",rohit_pass)
+
+if __name__ == "__main__" : # Restriction 
+    def manager_hi():
+        manager_id  = "Manager_boss@gmail.com"
+        manager_pass = 'boss123456'
+        print("Manager name : Dharmapal")
+        print("Manager id : ",manager_id)
+        print("Manager password : ",manager_pass)
+        
