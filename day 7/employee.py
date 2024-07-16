50cc2cf66d93c12ec564737b92d78a107ad2e4ee
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-16 09:37:01 +0530

    Create employee.py

---
+++ day 7/employee.py
@@ -0,0 +1,29 @@
+# Rohit
+import manager
+
+print(manager.rohit_id)
+print(manager.rohit_pass)
+
+print(manager.employe_list)
+
+# calling
+manager.rohit_hi()
+
+# Nick name
+
+import manager as mng # alias
+print(mng.rohit_id)
+print(mng.rohit_pass)
+
+print(mng.employe_list)
+
+import manager as mng
+print(mng.manager_name)
+
+import manager
+
+print(manager.rohit_id)
+print(manager.rohit_pass)
+print()
+manager.rohit_hi()
+print(manager.manager_name)
