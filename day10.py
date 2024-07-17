2a1257ab24f8cf9688d0e085849bd0221da6152c
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-17 09:55:49 +0530

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,7 @@
+# Create a program in python to send the messages from sender to the receiver
+# Use socket for making the connection from sender to receiver
+
+# Further modify the program to also send the message from receiver to sender
+# Can use both the UDP/TCP protocol
+
+# Atlast also try to modify the code to send the files,pdf etc from sender to receiver.
