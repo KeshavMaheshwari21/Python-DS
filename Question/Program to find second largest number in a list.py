94716d0ef10f73be40d224b33701d1a6df1f5223
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-19 19:52:55 +0530

    Create Program to find second largest number in a list.py

---
+++ Question/Program to find second largest number in a list.py
@@ -0,0 +1,11 @@
+# List of numbers
+list1 = [10, 20, 20, 4, 45, 45, 45, 99, 99]
+ 
+# Removing duplicates from the list
+list2 = list(set(list1))
+ 
+# Sorting the  list
+list2.sort()
+ 
+# Printing the second last element
+print("Second largest element is:", list2[-2])
