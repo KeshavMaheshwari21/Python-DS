e955a073d84d69f55b7b700051446ba08beba779
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-20 11:24:30 +0530

    Create Program to Swap Two Elements in a List.py

---
+++ Question/Program to Swap Two Elements in a List.py
@@ -0,0 +1,9 @@
+def swapPositions(list, pos1, pos2):
+     
+    list[pos1], list[pos2] = list[pos2], list[pos1]
+    return list
+
+List = [23, 65, 19, 90]
+pos1, pos2  = 1, 3
+ 
+print(swapPositions(List, pos1-1, pos2-1))
