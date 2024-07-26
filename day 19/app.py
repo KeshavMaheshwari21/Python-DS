fc6038e823c508a58a95e7db106e70ac8a3380d2
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-07-27 00:07:47 +0530

    Add files via upload

---
+++ day 19/app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
